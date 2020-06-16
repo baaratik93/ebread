3b43844e99889101c60f49358f9d80deeab821f7
Mamadou Thiam <dooro.mah@gmail.com>
2020-06-16 20:22:29 +0000

    Test de la base donnée qui est undefined

--- routes/boulanger.js
+++ routes/boulanger.js
@@ -11,12 +11,32 @@ router.get('/new',(req,res)=>{
     res.render('boulangers/new',{ boulanger: new Boulanger() })
 })
 //  Ajouter un boulanger à la BD
-router.post('/',(req,res)=>{
-    res.send(new Boulanger({
+router.post('/',async(req,res)=>{
+    let boulanger = new Boulanger({
         nom:  req.body.nom,
         telephone: req.body.telephone,
         adress: req.body.adress
-    }))
+    })
+    try {
+        let newBoulanger = await boulanger.save()
+        res.redirect('boulangers')
+    } catch  {
+                res.render('boulangers/new', {
+                boulanger: boulanger,
+                msgErr: "Erreur d'enrégistrement à la BD"
+            })
+    }
+    // boulanger.save((err,newBoulanger)=>{
+    //     if(err){
+    //         res.render('boulangers/new', {
+    //             boulanger: boulanger,
+    //             msgErr: "Erreur d'enrégistrement à la BD"
+    //         })
+    //     }else
+    //     {
+    //         res.redirect('boulangers')
+    //     }
+    // })
 })
 
 module.exports = router
